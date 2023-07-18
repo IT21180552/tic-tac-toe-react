131fb6335f51f07b5ecc550116a64ee6233111df
IT21180552 <it21180552@my.sliit.lk>
2023-07-18 06:13:56 +0530

    files and implement App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import required components
+import Board from './Board';
+import Info from './Info';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './css/App.css';
+import {useState} from 'react';
+
+function App(){
+    const [rest,setState] = useState(false);
+    const [winner,setWinner] = useState('');
+
+    const restBoard = () => {
+        setReset(true);
+    }
+
+    return(
+        <div className="App">
+        {/* Shrinks the popup when there is no winner */}
+        <div className={`winner ${winner !== '' ? '' : 'shrink'}`}>
+            {/* Display the current winner */}
+            <div className='winner-text'>{winner}</div>
+            {/* Button used to reset the board */}
+            <button onClick={() => resetBoard()}>
+                Reset Board
+            </button>
+        </div>
+        {/* Custom made board component comprising of
+        the tic-tac-toe board  */}
+        <Board reset={reset} setReset={setReset} winner={winner}
+            setWinner={setWinner} />
+        <Info />
     </div>
-  );
+    );
 }
 
 export default App;
